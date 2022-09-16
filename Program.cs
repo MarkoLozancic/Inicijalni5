75c6ceb2cbe23ed93b00f8314cddf73f8623d0b2
User <User@LOZA>
2022-09-16 12:59:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni5
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string url;
+            Console.WriteLine("Unesite URL da provjerim je li u ispravnom formatu: ");
+           url=Console.ReadLine();
+            if(url.StartsWith("www.")&&url.EndsWith(".hr"))
+            {
+                Console.WriteLine("URL je ispravan!");
+            }
+            else
+            { Console.WriteLine("URL je neispravan!"); }
+            Console.ReadKey();
+        }
+    }
+}
